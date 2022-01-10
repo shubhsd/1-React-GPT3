2b04b667d0444343671a2dae05a43d35d1c5d187
Shubham <shubhsd@users.noreply.github.com>
2022-01-10 19:11:59 +0530

    Project setup done and basic css in index.js and app.css added
    Project setup done and basic css in index.js and app.css added

---
+++ src/components/callToAction/CallToAction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './callToAction.css';
+
+const CallToAction = () => {
+    return (
+        <div>
+            CallToAction
+        </div>
+    )
+}
+
+export default CallToAction
